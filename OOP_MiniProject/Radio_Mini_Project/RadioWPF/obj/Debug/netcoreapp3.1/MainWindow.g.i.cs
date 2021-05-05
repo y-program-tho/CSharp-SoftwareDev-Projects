48e61136a6c3a51fc3569c879e34ce44667dd3eb
YangaL96 <ylomuro12@gmail.com>
2021-05-05 12:07:41 +0100

    Visual Studios adding running files

--- OOP_MiniProject/Radio_Mini_Project/RadioWPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ OOP_MiniProject/Radio_Mini_Project/RadioWPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace RadioWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RadioWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RadioWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- WPFCalculatorApp/WPFCalculatorApp/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ WPFCalculatorApp/WPFCalculatorApp/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace WPFCalculatorApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFCalculatorApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFCalculatorApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
